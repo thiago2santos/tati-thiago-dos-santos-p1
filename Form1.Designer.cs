abacad7f09bef20f3ffd497c3546adb553702675
thiago2santos <thisanti@hotmail.com>
2025-03-18 23:54:43 -0300

    Criado um client http com ajuda do copilot

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace tati_thiago_dos_santos_p1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGET = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGET
+            // 
+            this.btnGET.Location = new System.Drawing.Point(197, 86);
+            this.btnGET.Name = "btnGET";
+            this.btnGET.Size = new System.Drawing.Size(75, 23);
+            this.btnGET.TabIndex = 0;
+            this.btnGET.Text = "button1";
+            this.btnGET.UseVisualStyleBackColor = true;
+            this.btnGET.Click += new System.EventHandler(this.btnGET_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGET);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGET;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
-namespace tati_thiago_dos_santos_p1
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+namespace tati_thiago_dos_santos_p1 {
+    public partial class Form1 : Form {
+        public Form1() {
             InitializeComponent();
+        }
+
+        private async void btnGET_Click(object sender, EventArgs e) {
+            string url = "https://reqres.in/api/users?page=2";
+            using (HttpClient client = new HttpClient()) {
+                try {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    UserResponse userResponse = JsonConvert.DeserializeObject<UserResponse>(responseBody);
+                    MessageBox.Show($"Total de usuários: {userResponse.Total}", "Response", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                } catch (HttpRequestException ex) {
+                    MessageBox.Show($"Erro na requisição: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
