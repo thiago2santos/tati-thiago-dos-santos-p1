a29ca79acf7158a29f2542a8451890b37434e74d
thiago2santos <thisanti@hotmail.com>
2025-04-12 18:45:30 -0300

    Refatorar interface e lógica de requisição assíncrona
    Substituído o controle `tabControl1` por `tabControl` com a adição da aba `tabPage3`. Implementados novos componentes, incluindo um `GroupBox` e três `ComboBox` para filtragem de dados na `ListView`. Ajustadas dimensões e layout de controles como `ListView`, `ProgressBar` e `MenuStrip`.
    
    Atualizada a lógica de requisição para utilizar `async/await`, removendo o `BackgroundWorker`. Métodos adicionados para preencher os `ComboBox` com dados da API e filtrar a `ListView` com base nas seleções. Comentários e código desnecessário foram removidos para melhorar a legibilidade.

--- formularios/WarSpoting.Designer.cs
+++ formularios/WarSpoting.Designer.cs
@@ -32,21 +32,30 @@
             this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lblData = new System.Windows.Forms.Label();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.listView = new System.Windows.Forms.ListView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.grpBox = new System.Windows.Forms.GroupBox();
+            this.cBoxTipo = new System.Windows.Forms.ComboBox();
+            this.cBoxModelo = new System.Windows.Forms.ComboBox();
+            this.cBoxStatus = new System.Windows.Forms.ComboBox();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.menuStrip.SuspendLayout();
-            this.tabControl1.SuspendLayout();
+            this.tabControl.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.grpBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // dateTimePicker
             // 
             this.dateTimePicker.Location = new System.Drawing.Point(9, 50);
-            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(2);
             this.dateTimePicker.Name = "dateTimePicker";
             this.dateTimePicker.Size = new System.Drawing.Size(220, 20);
             this.dateTimePicker.TabIndex = 2;
@@ -56,10 +65,10 @@
             this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(9, 372);
-            this.listBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox.Location = new System.Drawing.Point(12, 372);
+            this.listBox.Margin = new System.Windows.Forms.Padding(2);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(938, 69);
+            this.listBox.Size = new System.Drawing.Size(920, 69);
             this.listBox.TabIndex = 3;
             // 
             // progressBar
@@ -67,9 +76,9 @@
             this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar.Location = new System.Drawing.Point(10, 446);
-            this.progressBar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(2);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(935, 19);
+            this.progressBar.Size = new System.Drawing.Size(922, 19);
             this.progressBar.TabIndex = 4;
             // 
             // btnConsultarPerdas
@@ -96,7 +105,7 @@
             this.arquivoToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
-            this.menuStrip.Size = new System.Drawing.Size(963, 24);
+            this.menuStrip.Size = new System.Drawing.Size(950, 24);
             this.menuStrip.TabIndex = 6;
             this.menuStrip.Text = "menuStrip1";
             // 
@@ -125,15 +134,19 @@
             this.lblData.TabIndex = 7;
             this.lblData.Text = "Selecione uma data";
             // 
-            // tabControl1
+            // tabControl
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(10, 76);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(897, 247);
-            this.tabControl1.TabIndex = 8;
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Controls.Add(this.tabPage3);
+            this.tabControl.Location = new System.Drawing.Point(10, 76);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(928, 291);
+            this.tabControl.TabIndex = 8;
             // 
             // tabPage1
             // 
@@ -141,10 +154,23 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(889, 221);
+            this.tabPage1.Size = new System.Drawing.Size(920, 265);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Detalhes";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(5, 5);
+            this.listView.Margin = new System.Windows.Forms.Padding(2);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(910, 255);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
             // 
             // tabPage2
             // 
@@ -156,18 +182,6 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Localização";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // listView
-            // 
-            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView.HideSelection = false;
-            this.listView.Location = new System.Drawing.Point(5, 5);
-            this.listView.Margin = new System.Windows.Forms.Padding(2);
-            this.listView.Name = "listView";
-            this.listView.Size = new System.Drawing.Size(879, 211);
-            this.listView.TabIndex = 1;
-            this.listView.UseCompatibleStateImageBehavior = false;
             // 
             // webBrowser
             // 
@@ -183,12 +197,91 @@
             this.webBrowser.TabIndex = 0;
             this.webBrowser.Url = new System.Uri("", System.UriKind.Relative);
             // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(889, 221);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Donate";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // grpBox
+            // 
+            this.grpBox.Controls.Add(this.lblStatus);
+            this.grpBox.Controls.Add(this.lblModelo);
+            this.grpBox.Controls.Add(this.lblTipo);
+            this.grpBox.Controls.Add(this.cBoxStatus);
+            this.grpBox.Controls.Add(this.cBoxModelo);
+            this.grpBox.Controls.Add(this.cBoxTipo);
+            this.grpBox.Location = new System.Drawing.Point(355, 27);
+            this.grpBox.Name = "grpBox";
+            this.grpBox.Size = new System.Drawing.Size(519, 65);
+            this.grpBox.TabIndex = 9;
+            this.grpBox.TabStop = false;
+            this.grpBox.Text = "Filtrar";
+            // 
+            // cBoxTipo
+            // 
+            this.cBoxTipo.FormattingEnabled = true;
+            this.cBoxTipo.Location = new System.Drawing.Point(6, 38);
+            this.cBoxTipo.Name = "cBoxTipo";
+            this.cBoxTipo.Size = new System.Drawing.Size(119, 21);
+            this.cBoxTipo.TabIndex = 0;
+            this.cBoxTipo.SelectedIndexChanged += new System.EventHandler(this.cBoxTipo_SelectedIndexChanged);
+            // 
+            // cBoxModelo
+            // 
+            this.cBoxModelo.FormattingEnabled = true;
+            this.cBoxModelo.Location = new System.Drawing.Point(203, 38);
+            this.cBoxModelo.Name = "cBoxModelo";
+            this.cBoxModelo.Size = new System.Drawing.Size(119, 21);
+            this.cBoxModelo.TabIndex = 1;
+            this.cBoxModelo.SelectedIndexChanged += new System.EventHandler(this.cBoxModelo_SelectedIndexChanged);
+            // 
+            // cBoxStatus
+            // 
+            this.cBoxStatus.FormattingEnabled = true;
+            this.cBoxStatus.Location = new System.Drawing.Point(394, 38);
+            this.cBoxStatus.Name = "cBoxStatus";
+            this.cBoxStatus.Size = new System.Drawing.Size(119, 21);
+            this.cBoxStatus.TabIndex = 2;
+            this.cBoxStatus.SelectedIndexChanged += new System.EventHandler(this.cBoxStatus_SelectedIndexChanged);
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Location = new System.Drawing.Point(7, 19);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(28, 13);
+            this.lblTipo.TabIndex = 3;
+            this.lblTipo.Text = "Tipo";
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(227, 16);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(42, 13);
+            this.lblModelo.TabIndex = 4;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(391, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(37, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            // 
             // WarSpoting
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(963, 480);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(950, 480);
+            this.Controls.Add(this.grpBox);
+            this.Controls.Add(this.tabControl);
             this.Controls.Add(this.lblData);
             this.Controls.Add(this.btnConsultarPerdas);
             this.Controls.Add(this.progressBar);
@@ -196,14 +289,16 @@
             this.Controls.Add(this.dateTimePicker);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "WarSpoting";
             this.Text = "War Spoting";
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
-            this.tabControl1.ResumeLayout(false);
+            this.tabControl.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
+            this.grpBox.ResumeLayout(false);
+            this.grpBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,10 +314,18 @@
         private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
         private System.Windows.Forms.Label lblData;
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tabControl;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.ListView listView;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox grpBox;
+        private System.Windows.Forms.ComboBox cBoxTipo;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.ComboBox cBoxStatus;
+        private System.Windows.Forms.ComboBox cBoxModelo;
     }
 }
--- formularios/WarSpoting.cs
+++ formularios/WarSpoting.cs
@@ -6,6 +6,8 @@ using Newtonsoft.Json;
 
 namespace tati_thiago_dos_santos_p1 {
     public partial class WarSpoting : Form {
+        private LossResponse lossResponse { get; set; }
+
         public WarSpoting() {
             InitializeComponent();
             listView.View = View.Details;
@@ -15,9 +17,7 @@ namespace tati_thiago_dos_santos_p1 {
             listView.Columns.Add("Tipo", 150);
             listView.Columns.Add("Modelo", 150);
             listView.Columns.Add("Status", 100);
-            //listView.Columns.Add("Perdido por", 100);
             listView.Columns.Add("Data", 100);
-            //listView.Columns.Add("Local mais próximo", 100);
             listView.Columns.Add("Geo", 100);
             listView.Columns.Add("Unidade", 100);
             listView.Columns.Add("Tags", 150);
@@ -26,7 +26,7 @@ namespace tati_thiago_dos_santos_p1 {
         private async Task<LossResponse> GetLossesAsync(string date) {
             string url = $"https://ukr.warspotting.net/api/losses/russia/{date}/";
             using (HttpClient client = new HttpClient()) {
-                LossResponse lossResponse = null;
+                //LossResponse lossResponse = null;
                 client.Timeout = TimeSpan.FromSeconds(3);
                 try {
                     HttpResponseMessage response = await client.GetAsync(url);
@@ -50,9 +50,7 @@ namespace tati_thiago_dos_santos_p1 {
             listBox.Items.Add($"Consultando perdas para a data: {formattedDate}");
 
             try {
-                bgWorker.RunWorkerAsync(); // Inicia o processamento em segundo plano
                 LossResponse lossResponse = await GetLossesAsync(formattedDate);
-                bgWorker.CancelAsync();
 
                 if (lossResponse != null && lossResponse.Losses.Count != 0) {
                     progressBar.Value = 0;
@@ -60,14 +58,13 @@ namespace tati_thiago_dos_santos_p1 {
                     progressBar.Maximum = lossResponse.Losses.Count;
 
                     listBox.Items.Add("Dados recebidos. Preenchendo ListView...");
+
                     foreach (var loss in lossResponse.Losses) {
                         ListViewItem item = new ListViewItem(loss.Id.ToString());
                         item.SubItems.Add(loss.Type);
                         item.SubItems.Add(loss.Model);
                         item.SubItems.Add(loss.Status);
-                        //item.SubItems.Add(loss.LostBy);
                         item.SubItems.Add(loss.Date.ToString("yyyy-MM-dd"));
-                        //item.SubItems.Add(loss.NearestLocation);
                         item.SubItems.Add(loss.Geo);
                         item.SubItems.Add(loss.Unit ?? "N/A");
                         item.SubItems.Add(loss.Tags != null ? string.Join(", ", loss.Tags) : "N/A");
@@ -75,6 +72,9 @@ namespace tati_thiago_dos_santos_p1 {
                         progressBar.Value++;
                     }
                     listBox.Items.Add("ListView preenchido com sucesso.");
+
+                    PreencherFiltros(lossResponse);
+
 
                 } else if (lossResponse != null && lossResponse.Losses.Count == 0) {
                     listBox.Items.Add("Nenhuma perda encontrada para a data selecionada.");
@@ -89,6 +89,88 @@ namespace tati_thiago_dos_santos_p1 {
 
         private void sairToolStripMenuItem_Click(object sender, EventArgs e) {
             Application.Exit();
+        }
+        private void PreencherFiltros(LossResponse lossResponse) {
+            // Preenche o ComboBox de Tipo
+            cBoxTipo.Items.Clear();
+            cBoxTipo.Items.Add("Todos");
+            lossResponse.Losses.ForEach(l => {
+                if (!cBoxTipo.Items.Contains(l.Type)) {
+                    cBoxTipo.Items.Add(l.Type);
+                }
+            });
+            cBoxTipo.SelectedIndex = 0;
+
+            // Preenche o ComboBox de Modelo
+            cBoxModelo.Items.Clear();
+            cBoxModelo.Items.Add("Todos");
+            lossResponse.Losses.ForEach(l => {
+                if (!cBoxModelo.Items.Contains(l.Model)) {
+                    cBoxModelo.Items.Add(l.Model);
+                }
+            });
+            cBoxModelo.SelectedIndex = 0;
+
+            // Preenche o ComboBox de Status
+            cBoxStatus.Items.Clear();
+            cBoxStatus.Items.Add("Todos");
+            lossResponse.Losses.ForEach(l => {
+                if (!cBoxStatus.Items.Contains(l.Status)) {
+                    cBoxStatus.Items.Add(l.Status);
+                }
+            });
+            cBoxStatus.SelectedIndex = 0;
+        }
+
+        private void cBoxTipo_SelectedIndexChanged(object sender, EventArgs e) {
+            listView.Items.Clear();
+            foreach (var loss in lossResponse.Losses) {
+                if (cBoxTipo.SelectedItem.ToString() == "Todos" || loss.Type == cBoxTipo.SelectedItem.ToString()) {
+                    ListViewItem item = new ListViewItem(loss.Id.ToString());
+                    item.SubItems.Add(loss.Type);
+                    item.SubItems.Add(loss.Model);
+                    item.SubItems.Add(loss.Status);
+                    item.SubItems.Add(loss.Date.ToString("yyyy-MM-dd"));
+                    item.SubItems.Add(loss.Geo);
+                    item.SubItems.Add(loss.Unit ?? "N/A");
+                    item.SubItems.Add(loss.Tags != null ? string.Join(", ", loss.Tags) : "N/A");
+                    listView.Items.Add(item);
+                }
+            }
+        }
+
+        private void cBoxModelo_SelectedIndexChanged(object sender, EventArgs e) {
+            listView.Items.Clear();
+            foreach (var loss in lossResponse.Losses) {
+                if (cBoxModelo.SelectedItem.ToString() == "Todos" || loss.Model == cBoxModelo.SelectedItem.ToString()) {
+                    ListViewItem item = new ListViewItem(loss.Id.ToString());
+                    item.SubItems.Add(loss.Type);
+                    item.SubItems.Add(loss.Model);
+                    item.SubItems.Add(loss.Status);
+                    item.SubItems.Add(loss.Date.ToString("yyyy-MM-dd"));
+                    item.SubItems.Add(loss.Geo);
+                    item.SubItems.Add(loss.Unit ?? "N/A");
+                    item.SubItems.Add(loss.Tags != null ? string.Join(", ", loss.Tags) : "N/A");
+                    listView.Items.Add(item);
+                }
+            }
+        }
+
+        private void cBoxStatus_SelectedIndexChanged(object sender, EventArgs e) {
+            listView.Items.Clear();
+            foreach (var loss in lossResponse.Losses) {
+                if (cBoxStatus.SelectedItem.ToString() == "Todos" || loss.Status == cBoxStatus.SelectedItem.ToString()) {
+                    ListViewItem item = new ListViewItem(loss.Id.ToString());
+                    item.SubItems.Add(loss.Type);
+                    item.SubItems.Add(loss.Model);
+                    item.SubItems.Add(loss.Status);
+                    item.SubItems.Add(loss.Date.ToString("yyyy-MM-dd"));
+                    item.SubItems.Add(loss.Geo);
+                    item.SubItems.Add(loss.Unit ?? "N/A");
+                    item.SubItems.Add(loss.Tags != null ? string.Join(", ", loss.Tags) : "N/A");
+                    listView.Items.Add(item);
+                }
+            }
         }
     }
 }
